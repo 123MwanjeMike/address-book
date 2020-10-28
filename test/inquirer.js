d775fe7c0ad2e9e09e4c65816c46df35f4893f9e
Mike Mwanje <mwanjemike767@gmail.com>
2020-10-28 17:35:14 +0300

    expect inquirer tests

--- test/inquirer.js
+++ test/inquirer.js
@@ -1,28 +1,28 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
 const { describe, it } = require('mocha');
 const sinon = require('sinon');
 const inquirer = require('../inquirer');
 
-describe('app.js tests', () => {
+describe('inquirer.js tests', () => {
   const menuStub = sinon.stub(inquirer, 'menu');
   const nameStub = sinon.stub(inquirer, 'name');
   const telephoneStub = sinon.stub(inquirer, 'telephone');
   const navigateStub = sinon.stub(inquirer, 'navigate');
 
   it('inquirer.menu calls menu()', async () => {
-    inquirer.menu();
-    assert.isTrue(menuStub.called);
+    menuStub.onCall(0).returns({ option: 'Add contact' });
+    expect(inquirer.menu()).to.deep.equal({ option: 'Add contact' });
   });
   it('inquirer.name calls name()', async () => {
-    inquirer.name();
-    assert.isTrue(nameStub.called);
+    nameStub.onCall(0).returns({ name: 'Mike' });
+    expect(inquirer.name()).to.deep.equal({ name: 'Mike' });
   });
   it('inquirer.telephone calls telephone()', async () => {
-    inquirer.telephone();
-    assert.isTrue(telephoneStub.called);
+    telephoneStub.onCall(0).returns({ telephone: '07777777777' });
+    expect(inquirer.telephone()).to.deep.equal({ telephone: '07777777777' });
   });
   it('inquirer.navigate calls navigate()', async () => {
-    inquirer.navigate();
-    assert.isTrue(navigateStub.called);
+    navigateStub.onCall(0).returns({ navigate: 'Yes' });
+    expect(inquirer.navigate()).to.deep.equal({ navigate: 'YesMikeMike' });
   });
 });
